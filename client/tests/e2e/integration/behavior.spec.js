98cafbaa569443c68ef914ca00cfba7f400fcb5a
Agostina Fassio <agostinafassio@gmail.com>
2020-12-18 20:50:44 -0300

    Testing

---
+++ client/tests/e2e/integration/behavior.spec.js
@@ -0,0 +1,37 @@
+it("loads", () => {
+  cy.visit("/");
+});
+
+it("shows search bar", () => {
+  cy.visit("/")
+    .get("#search input");
+});
+
+it("doesn't redirect if search bar is empty", () => {
+  cy.visit("/");
+  cy.get("#search input").click();
+  cy.url().should('not.include', 'search');
+});
+
+it("gets results while searching", () => {
+  cy.visit("/");
+  cy.get("#search input").type('celular');
+  cy.get("#search span").click();
+  cy.get(".result_row");
+});
+
+it("navigates to detail page when clicking on result", () => {
+  cy.visit("/");
+  cy.get("#search input").type('celular');
+  cy.get("#search span").click();
+  cy.get(".result_row:first .title").click();
+  cy.url().should('include', '/items/');
+});
+
+it("gets details when clicking on result", () => {
+  cy.visit("/");
+  cy.get("#search input").type('celular');
+  cy.get("#search span").click();
+  cy.get(".result_row:first .title").click();
+  cy.get("#detail_box_head .title");
+});
